4bb61da66f2e8b12436cf500c3694faa528ea09a
jsung5381 <jsung5381@naver.com>
2018-04-30 16:38:14 +0000

    It now supports vim-plug

--- config.vim
+++ config.vim
@@ -1,3 +1,12 @@
+" Plugins will be downloaded under the specified directory.
+call plug#begin('~/.vim/plugged')
+
+" Declare the list of plugins.
+Plug 'tpope/vim-sensible'
+
+" List ends here. Plugins become visible to Vim after this call.
+call plug#end()
+
 " INDENT
 set autoindent
 set smartindent
--- setup.sh
+++ setup.sh
@@ -1,1 +1,36 @@
-cp -bs $(pwd)/config.vim ~/.vimrc
+#!/bin/bash
+
+function install_plugin_manager {
+	# install vim-plug if not installed
+	if [ ! -f ~/.vim/autoload/plug.vim ]; then
+		uri=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs $uri
+		echo "the vim plugin manager is installed!"
+	fi
+}
+
+function copy_vimrc {
+	# set config.vim path
+	if [ ! $1 ]; then cpath=$PWD; else cpath=$1; fi
+
+	# if no pre-installed .vimrc, then
+	if [ ! -f ~/.vimrc ]
+	then
+		# simply copy config.vim as .vimrc for current system
+		cp -s $cpath/config.vim ~/.vimrc
+	# else, if there is a pre-installed .vimrc
+	else
+		# check if it's identical, if not, install one
+		cmp -s $cpath/config.vim ~/.vimrc || cp -bs $cpath/config.vim ~/.vimrc 
+	fi
+	echo "the vim configuration file is now set!"
+}
+
+function main {
+	install_plugin_manager
+	copy_vimrc
+	exit 0
+}
+
+main
+
