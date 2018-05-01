9164c7eee9f0fdbf399d79f645608468ce733b15
jsung5381 <jsung5381@naver.com>
2018-05-01 17:34:26 +0000

    Splitted the cases so that it now handles mac and linux env

--- config.vim
+++ config.vim
@@ -10,6 +10,7 @@ call plug#end()
 " INDENT
 set autoindent
 set smartindent
+set expandtab
 set tabstop=4
 set softtabstop=4
 set shiftwidth=4
--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-CONFIG=$PWD/config.vim	# absolute path to source config file
-VIMRC=~/.vimrc			# path to destination vimrc file
+declare -r CONFIG=$PWD/config.vim	# absolute path to source config file
+declare -r VIMRC=$HOME/.vimrc	    # absolute path to destination vimrc file
 
-E_XCURL=66				# cannot download from the url
-E_XOS=67				# cannot detect ostype
-E_XCP=68				# cannot perform cp
+readonly E_XCURL=66				# cannot download from the url
+readonly E_XOS=67				# cannot detect ostype
+readonly E_XCP=68				# cannot perform cp
 
 platform='unknown'	
 
 install_plugin_manager() {
-	# install vim-plug if not installed
+	# install vim-plug, if not installed
 	if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
 		url=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 		curl -fLo ~/.vim/autoload/plug.vim --create-dirs $url || {
-			echo 'The vim plugin manager was not installed.' >&2
+			echo 'Installing the vim plugin manager is failed.' >&2
 			exit $E_XCURL
 		}
 		echo 'The vim plugin manager is installed!'
@@ -22,42 +22,50 @@ install_plugin_manager() {
 }
 
 detect_ostype() {
-	case "$OSTYPE" in
+	case $OSTYPE in
 		darwin*	) platform='mac';;
 		linux*	) platform='linux';;
 		*		) echo "unknown: $OSTYPE"; exit $E_XOS;;
 	esac
 }
 
-copy_vimrc() {
+link_vimrc() {
 
 	# if no pre-installed .vimrc, then
 	if [[ ! -f $VIMRC ]]; then
-
 		# simply copy config.vim as .vimrc for current system
-		cp -s $CONFIG $VIMRC || {
-			echo 'Cannot perform cp.' >&2
-			exit $E_XCP;
-		}
-	# else, if there is a pre-installed .vimrc
+        copy_vimrc_linux
+	
+    # else, if there is a pre-installed .vimrc
 	else
-		# check if it's identical, if not, install one
+		# check if they're identical, if not, install one
 		if ( cmp --silent $CONFIG $VIMRC ); then
 			echo 'The most recent .vimrc is already installed in your machine.'
 		else
-			cp -bs $CONFIG $VIMRC || {
-				echo 'Cannot perform cp.' >&2
-				exit $E_XCP;
-			}
-			echo 'The .vimrc is now installed in your machine!'
+            copy_vimrc_linux
 		fi
 	fi
 }
 
+copy_vimrc_linux() {
+    cp -bs $CONFIG $VIMRC || {
+        echo "Cannot create symbolic link to $CONFIG" >&2
+        exit $E_XCP;
+    }
+    echo 'The .vimrc is now installed in your machine!'
+}
+
+copy_vimrc_mac() {
+    ln -s $CONFIG $VIMRC || {
+        echo "Cannot create symbolic link to $CONFIG" >&2
+        exit $E_XCP;
+    }
+}
+
 main() {
-	install_plugin_manager
-	detect_ostype
-	copy_vimrc
+    install_plugin_manager
+    detect_ostype
+    link_vimrc
 }
 
 main
