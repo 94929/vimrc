f342ba4adbbd72abb77a42837a1f5843e184d108
jsung5381 <jsung5381@naver.com>
2018-05-01 16:40:56 +0000

    Added way of handling exceptions

--- setup.sh
+++ setup.sh
@@ -1,35 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+CONFIG=$PWD/config.vim	# absolute path to source config file
+VIMRC=~/.vimrc			# path to destination vimrc file
+
+E_XCURL=66				# cannot download from the url
+E_XOS=67				# cannot detect ostype
+E_XCP=68				# cannot perform cp
+
+platform='unknown'	
 
 install_plugin_manager() {
 	# install vim-plug if not installed
-	if [ ! -f ~/.vim/autoload/plug.vim ]; then
-		uri=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-		curl -fLo ~/.vim/autoload/plug.vim --create-dirs $uri
-		echo "the vim plugin manager is installed!"
+	if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
+		url=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs $url || {
+			echo 'The vim plugin manager was not installed.' >&2
+			exit $E_XCURL
+		}
+		echo 'The vim plugin manager is installed!'
 	fi
 }
 
+detect_ostype() {
+	case "$OSTYPE" in
+		darwin*	) platform='mac';;
+		linux*	) platform='linux';;
+		*		) echo "unknown: $OSTYPE"; exit $E_XOS;;
+	esac
+}
+
 copy_vimrc() {
-	# set config.vim path
-	if [ ! $1 ]; then cpath=$PWD; else cpath=$1; fi
 
 	# if no pre-installed .vimrc, then
-	if [ ! -f ~/.vimrc ]
-	then
+	if [[ ! -f $VIMRC ]]; then
+
 		# simply copy config.vim as .vimrc for current system
-		cp -s $cpath/config.vim ~/.vimrc
+		cp -s $CONFIG $VIMRC || {
+			echo 'Cannot perform cp.' >&2
+			exit $E_XCP;
+		}
 	# else, if there is a pre-installed .vimrc
 	else
 		# check if it's identical, if not, install one
-		cmp -s $cpath/config.vim ~/.vimrc || cp -bs $cpath/config.vim ~/.vimrc 
+		if ( cmp --silent $CONFIG $VIMRC ); then
+			echo 'The most recent .vimrc is already installed in your machine.'
+		else
+			cp -bs $CONFIG $VIMRC || {
+				echo 'Cannot perform cp.' >&2
+				exit $E_XCP;
+			}
+			echo 'The .vimrc is now installed in your machine!'
+		fi
 	fi
-	echo "the vim configuration file is now set!"
 }
 
 main() {
 	install_plugin_manager
+	detect_ostype
 	copy_vimrc
-	exit 0
 }
 
 main
