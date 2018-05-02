e48f3efc62dd0bc24424eee824ce3a5efc241c4e
jsung5381 <jsung5381@naver.com>
2018-05-02 09:19:45 +0000

    Added a method to install plugins

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ declare -r VIMRC=$HOME/.vimrc	    # absolute path to destination vimrc file
 readonly E_XCURL=66			# cannot download from the url
 readonly E_XOS=67				# cannot detect ostype
 readonly E_XCP=68				# cannot perform cp
-
-platform='unknown'	
+readonly E_XINS=69      # cannot install plugins
 
 install_plugin_manager() {
   # install vim-plug, if not installed
@@ -21,51 +20,50 @@ install_plugin_manager() {
 	fi
 }
 
-detect_ostype() {
-	case $OSTYPE in
-		darwin*	) platform='mac';;
-		linux*	) platform='linux';;
-		*		) echo "unknown: $OSTYPE"; exit $E_XOS;;
-	esac
-}
-
 link_vimrc() {
 
-	# if no pre-installed .vimrc, then
+  # if no pre-installed .vimrc, then
 	if [[ ! -f $VIMRC ]]; then
 		# simply copy config.vim as .vimrc for current system
-        copy_vimrc_linux
+    copy_vimrc
 	
     # else, if there is a pre-installed .vimrc
 	else
 		# check if they're identical, if not, install one
+    cmp --silent $CONFIG $VIMRC || copy_vimrc
 		if ( cmp --silent $CONFIG $VIMRC ); then
 			echo 'The most recent .vimrc is already installed in your machine.'
 		else
-            copy_vimrc_linux
+      copy_vimrc
 		fi
 	fi
 }
 
-copy_vimrc_linux() {
-    cp -bs $CONFIG $VIMRC || {
-        echo "Cannot create symbolic link to $CONFIG" >&2
-        exit $E_XCP;
-    }
-    echo 'The .vimrc is now installed in your machine!'
+copy_vimrc() {
+  case $OSTYPE in
+		darwin*	) cp="ln -s";;
+		linux*	) cp="cp -bs";;
+		*		    ) echo "unknown: $OSTYPE"; exit $E_XOS;;
+	esac
+
+  $cp $CONFIG $VIMRC || {
+    echo "Cannot create symbolic link to $CONFIG" >&2
+    exit $E_XCP;
+  }
+  echo 'The .vimrc is now installed in your machine!'
 }
 
-copy_vimrc_mac() {
-    ln -s $CONFIG $VIMRC || {
-        echo "Cannot create symbolic link to $CONFIG" >&2
-        exit $E_XCP;
-    }
+install_plugins() {
+  vim +PlugInstall +qall || {
+    echo 'Cannot install plugins' >&2
+    exit $E_XINS
+  }
 }
 
 main() {
-    install_plugin_manager
-    detect_ostype
-    link_vimrc
+  install_plugin_manager
+  link_vimrc
+  #install_plugins
 }
 
 main
